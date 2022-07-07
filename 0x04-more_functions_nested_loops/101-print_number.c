a887db2c4e480288c5c2d434406f5c931271bdf0
abrish-software <abrishfeker@gmail.com>
2022-07-07 02:11:25 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,11 @@
 */
 
 void print_number(int n)
-
 {
-
 unsigned int tens, digit, positive = n;
-
 double t_beg = 1;
 
 if (n == 0)
-
 _putchar('0');
 else
 {
@@ -23,9 +19,11 @@ if (n < 0)
 positive = n * -1;
 _putchar('-');
 }
+
 while (t_beg <= positive)
 t_beg *= 10;
 tens = t_beg / 10;
+
 while (tens >= 1)
 {
 digit = positive / tens;
