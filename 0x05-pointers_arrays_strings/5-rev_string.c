c9edd54cde9bfa8400ea1929439d3dc3bad791a5
abrish-software <abrishfeker@gmail.com>
2022-07-11 01:35:36 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,29 +7,25 @@
 */
 
 int _strlen(char *s)
-
 {
 int len = 0;
+
 while (*s != '\0')
 {
 len++;
 s++;
 }
+
 return (len);
 }
 
 void rev_string(char *s)
-
 {
-
 int i = 0;
-
 int j = (_strlen(s) - 1);
-
 char tmp;
 
 while (i < j)
-
 {
 tmp = s[i];
 s[i] = s[j];
