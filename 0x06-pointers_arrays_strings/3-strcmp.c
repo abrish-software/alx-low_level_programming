5a431d111e9d55ca6b52b1bc40ec32899a4638b0
abrish-software <97157856+abrish-software@users.noreply.github.com>
2022-07-14 11:24:43 +0300

    Update 3-strcmp.c
    comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,48 @@
 #include "main.h"
 
 /**
-*_strcmp - compare two strings
-*@s1: one string
-*@s2: one string
-*Return: int that tells num spaces in between
+* _strcmp - Compares two strings
+* @s1: The first string
+* @s2: The second string
+*Return: int value
 */
-
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
+int a = 0, b = 0, c = 0, r = 0, lim;
 
-while (*(s1 + i) == *(s2 + i) && *(s1 + i))
-i++;
-if (*(s2 + i))
-return (*(s1 + i) - *(s2 + i));
+while (s1[a])
+{
+a++;
+}
+
+while (s2[b])
+{
+b++;
+}
+
+if (a <= b)
+{
+lim = a;
+}
 else
-return (0);
+{
+lim = b;
+}
+
+while (c <= lim)
+{
+if (s1[c] == s2[c])
+{
+c++;
+continue;
+}
+else
+{
+r = s1[c] - s2[c];
+break;
+}
+
+c++;
+}
+return (r);
 }
